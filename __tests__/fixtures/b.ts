0f69469b2d1a7a435861e76f3dc742244f12f69c
noyobo <noyobo@gmail.com>
2024-09-06 11:11:30 +0800

    feat: support node env

--- __tests__/fixtures/b.ts
+++ __tests__/fixtures/b.ts
@@ -1,7 +1,10 @@
+import { join } from 'path';
+
 export default {
   name: 'b',
   age: 20,
   say(v: string) {
-    console.log('b say:', v);
+    console.log('b say:', v, join(__dirname, 'a.js'));
   },
+  b: join('/abc', 'a.js')
 };
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,40 @@
+import fs from 'node:fs';
+import { basename, dirname, extname, join } from 'node:path';
 import { buildSync } from 'esbuild';
 
-export function runCjs<T>(code: string) {
-  const module = { exports: {} };
-  const fn = new Function('module', 'exports', code);
-  fn(module, module.exports);
-  const mod = module.exports as T;
+export function runCjs<T>(filePath: string, code: string) {
+  const dir = dirname(filePath);
+  const fileName = basename(filePath);
+  const ext = extname(fileName);
+  const fileBase = `${fileName}.timestamp-${Date.now()}-${Math.random().toString(16).slice(2)}`;
+  const tmpFilePath = join(dir, fileBase + ext);
 
-  // @ts-ignore
-  if (mod?.__esModule) {
-    // @ts-ignore
-    return mod.default;
+  // TODO: support .mjs
+
+  fs.writeFileSync(tmpFilePath, code, 'utf-8');
+  const exports = require(tmpFilePath);
+  fs.unlinkSync(tmpFilePath);
+
+  if (exports.__esModule) {
+    return exports.default as T;
   }
-  return mod;
+  return exports as T;
 }
 
 /**
  * 读取并运行文件
  */
-export const requireModuleExports = (filename: string) => {
+export const requireModuleExports = (filePath: string) => {
   const result = buildSync({
-    entryPoints: [filename],
+    entryPoints: [filePath],
     bundle: true,
     minify: true,
+    platform: 'node',
     format: 'cjs',
     target: 'es2015',
     sourcemap: false,
     write: false,
   });
   const code = result.outputFiles[0].text;
-  return runCjs<Record<string, unknown>>(code);
+  return runCjs<Record<string, unknown>>(filePath, code);
 };
