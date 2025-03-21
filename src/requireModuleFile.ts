a813a4c727bb55b89fcac663fc0e85486fb4dd1b
noyobo <noyobo@gmail.com>
2025-03-21 12:30:02 +0800

    faet!: add requireModuleCode

--- src/requireModuleFile.ts
+++ src/requireModuleFile.ts
@@ -4,14 +4,7 @@ export function runCjs<T>(code: string) {
   const module = { exports: {} };
   const fn = new Function('module', 'exports', code);
   fn(module, module.exports);
-  const mod = module.exports as T;
-
-  // @ts-ignore
-  if (mod?.__esModule) {
-    // @ts-ignore
-    return mod.default;
-  }
-  return mod;
+  return module.exports as T;
 }
 
 /**
@@ -21,6 +14,24 @@ export function runCjs<T>(code: string) {
 export const requireModuleFile = (filePath: string) => {
   const result = buildSync({
     entryPoints: [filePath],
+    bundle: true,
+    minify: true,
+    format: 'cjs',
+    target: 'es2015',
+    sourcemap: false,
+    write: false,
+  });
+  const code = result.outputFiles[0].text;
+  return runCjs<Record<string, unknown>>(code);
+};
+
+export const requireModuleCode = (inputCode: string) => {
+  const result = buildSync({
+    stdin: {
+      contents: inputCode,
+      sourcefile: 'code.js',
+      loader: 'ts',
+    },
     bundle: true,
     minify: true,
     format: 'cjs',
