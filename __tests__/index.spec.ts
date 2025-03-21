3d601c3e8855d338dca7ef34afb53dcfcecf686f
noyobo <noyobo@gmail.com>
2025-03-21 12:30:02 +0800

    faet!: add requireModuleCode

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,11 +1,35 @@
 import * as path from 'node:path';
-import { loadModuleFile, requireModuleFile } from '../src/index';
+import { loadModuleFile, requireModuleCode, requireModuleFile } from '../src';
+import {describe, it, expect} from 'vitest';
 
 describe('index', () => {
   describe('require-module-exports', () => {
     it('basic', async () => {
-      expect(requireModuleFile(path.join(__dirname, './fixtures/a.js'))).toMatchSnapshot();
-      expect(loadModuleFile(path.join(__dirname, './fixtures/b.ts'))).toMatchSnapshot();
+      expect(requireModuleFile(path.join(__dirname, './fixtures/a.js'))).toMatchInlineSnapshot(`
+        {
+          "a": 1,
+        }
+      `);
+      expect(loadModuleFile(path.join(__dirname, './fixtures/b.ts'))).toMatchInlineSnapshot(`
+        {
+          "age": 20,
+          "b": "/abc/a.js",
+          "name": "b",
+          "say": [Function],
+        }
+      `);
+    });
+  });
+
+  describe('require-module-code', () => {
+    it('basic', async () => {
+      const code = `
+      export const a = 1;
+      export const b = 2;
+      `;
+      const result = requireModuleCode(code);
+      expect(result.a).toBe(1);
+      expect(result.b).toBe(2);
     });
   });
 });
